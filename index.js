dc88b628dfe5bf9851683b2128f4c6bb76690b2f
046159 <43796736+046159@users.noreply.github.com>
2024-02-26 18:28:52 +0000

    Added code to read input and create objects of the right type

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* ----------------------------- Setup constants ---------------------------- */
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,6 +11,142 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+/* --------------------- Function to initialise program --------------------- */
+function init() {
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'teamManagerName',
+                message: 'Team Manager - Name:',
+            },
+            {
+                type: 'input',
+                name: 'teamManagerId',
+                message: 'Team Manager - Employee ID:',
+            },
+            {
+                type: 'input',
+                name: 'teamManagerEmail',
+                message: 'Team Manager - Email:',
+            },
+            {
+                type: 'input',
+                name: 'teamManagerOfficeNumber',
+                message: 'Team Manager - Office Number:',
+            }
+        ])
+        .then((data) => {
+            const manager = new Manager(data.teamManagerName, data.teamManagerId, data.teamManagerEmail, data.teamManagerOfficeNumber);
+            displayEmployee("Manager", manager);
+            displayMenu();
+        })
+}
 
+function displayEmployee(type, data) {
+    console.log(data);
+}
+
+function displayMenu() {
+
+    console.log(`\n`);
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'menu',
+                message: 'Would you like to:',
+                choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
+            }
+        ])
+        .then((data) => {
+            console.log(`You chose ${data.menu}`);
+            switch (data.menu) {
+                case "Add an engineer":
+                    addEngineer();
+                    break;
+
+                case "Add an intern":
+                    addIntern();
+                    break;
+
+                case "Finish building the team":
+                    finish();
+                    break;
+
+                default:
+                    break;
+            }
+        })
+}
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: 'Engineer - Name:',
+            },
+            {
+                type: 'input',
+                name: 'engineerId',
+                message: 'Engineer - Employee ID:',
+            },
+            {
+                type: 'input',
+                name: 'engineerEmail',
+                message: 'Engineer - Email:',
+            },
+            {
+                type: 'input',
+                name: 'GitHub',
+                message: 'Engineer - GitHub Username:',
+            }
+        ])
+        .then((data) => {
+            const engineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.GitHub);
+            displayEmployee("Engineer", data);
+            displayMenu();
+        })
+}
+
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: 'Intern - Name:',
+            },
+            {
+                type: 'input',
+                name: 'internId',
+                message: 'Intern - Employee ID:',
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: 'Intern - Email:',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'Intern - School:',
+            }
+        ])
+        .then((data) => {
+            const intern = new Intern(data.internName, data.internId, data.internEmail, data.school);
+            displayEmployee("Intern", data);
+            displayMenu();
+        })
+}
+
+function finish() {
+    console.log(`Will finish`);
+}
+
+/* ------------------- Function call to initialise program ------------------ */
+console.clear();
+init();
